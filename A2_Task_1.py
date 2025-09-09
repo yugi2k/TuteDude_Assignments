c5b135978d018378df29ab21d7906c6ae2642e1a
yugi2k <yugi.2k12@gmail.com>
2025-09-09 16:00:53 -0400

    Add files via upload

---
+++ A2_Task_1.py
@@ -0,0 +1,11 @@
+# Check if a Number is Even or Odd
+
+
+#input
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+	print(f"{num} is a even number")
+else:
+	print(f"{num} is a odd number")
---
+++ A2_Task_2.py
@@ -0,0 +1,8 @@
+# sum of 1 to 50
+
+total_sum = 0
+
+for i in range(1, 51):
+	total_sum+=i
+
+print("The sum of numbers from 1 to 50 is: ", total_sum)
