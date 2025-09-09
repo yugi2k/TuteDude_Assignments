6cbe3465ae759f55da1f9bee7cacfec676159dad
yugi2k <yugi.2k12@gmail.com>
2025-09-09 10:36:01 -0400

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+# Create a Personalized Greeting
+
+# input from user
+
+f_name = str(input("Enter your first name: "))
+l_name = str(input("Enter your last name: "))
+
+# output
+print(f"Hello, {f_name} {l_name}! Welcome to the Python Program.")
